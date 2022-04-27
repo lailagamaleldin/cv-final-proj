1a464c8f6165dfcf41ffaa8ac98cb27912c3bbe4
Samaye Lohan <samayelohan98@gmail.com>
2022-04-27 15:52:08 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from Detector import *
+
+detector = Detector()
+
+detector.onImage("/Users/Samayelohan2/Desktop/Brown University/Spring 2022/CSCI 1430 - Computer Vision/FinalProject/images/baseline.jpg")
